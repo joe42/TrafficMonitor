0206b15ec6e5b9212afdfbf30e62b1aaef39da4a
joe42 <dna42@gmx.de>
2010-07-21 00:18:43 +0200

    Added GUI to configure traffic-warn-levels and URLs for traffic

--- chrome/content/interface.js
+++ chrome/content/interface.js
@@ -24,19 +24,21 @@ var trafficStatus = {
 	WarnLvl1: 0,
 	WarnLvl2: 0,
 	trafficURL: "",
-	trafficViewURL: ""
-   
-   
-    
-     
-	
+	trafficViewURL: "",
+	  
+	trafficViewURL2trafficURL: function( trafficViewURL ){
+		switch (arbeitstag) {
+			case "https://www.wh2.tu-dresden.de/usertraffic": 
+				return "http://www.wh2.tu-dresden.de/traffic/getMyTraffic.php";
+			default: return "http://www.wh2.tu-dresden.de/traffic/getMyTraffic.php";
+		}
 
-	
+	},
 	viewTrafficPage: function(){
 		this.newTab(this.trafficViewURL);
 	},
+	
    // Initialize the extension
-   },
 	init: function (viewElementID, viewAttribute) { 
 		//initialise observer for preferences dialog:
 		this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
@@ -46,45 +48,52 @@ var trafficStatus = {
 		this.prefs.addObserver("", this, false);
 		this.trafficURL = this.prefs.getCharPref("trafficURL");
 		this.trafficViewURL = this.prefs.getCharPref("trafficViewURL");
-		this.WarnLvl1 = this.prefs.getCharPref("WarnLvl1");
-		this.WarnLvl2 = this.prefs.getCharPref("WarnLvl2");
-		this.refreshInformation();    
-		window.setInterval(this.refreshInformation, 10*60*1000);
+		this.WarnLvl1 = this.prefs.getIntPref("WarnLvl1");
+		this.WarnLvl2 = this.prefs.getIntPref("WarnLvl2");  
 		
 		this.viewElement = document.getElementById( viewElementID );
 		this.viewAttribute = viewAttribute; 
 		this.getTraffic();
 		setInterval( "trafficStatus.getTraffic()", 60 * 1000 * 5 );
-	},observe: function(subject, topic, data)
-   {
-     if (topic != "nsPref:changed")
-     {
-       return;
-     }
- 
-     switch(data)
-     {
-       case "symbol":
-         this.tickerSymbol = this.prefs.getCharPref("symbol").toUpperCase();
-         this.refreshInformation();
-         break;
-     }
+	},
+	
+	observe: function(subject, topic, data){
+		if (topic != "nsPref:changed") {
+			return;
+		}
+  
+		switch(data) {
+			case "trafficViewURL":
+				this.trafficViewURL = this.prefs.getCharPref("trafficViewURL");  
+				this.trafficURL = trafficViewURL2trafficURL( this.trafficViewURL );
+				this.prefs.setCharPref( "trafficURL", this.trafficURL );//store as default
+				this.getTraffic();
+				break; 
+			case "WarnLvl1":
+				this.WarnLvl1 = this.prefs.getCharPref("WarnLvl1"); 
+				this.getTraffic();
+				break;
+			case "WarnLvl2":
+				this.WarnLvl2 = this.prefs.getCharPref("WarnLvl2"); 
+				this.getTraffic();
+				break;
+		}
    },
 
 
-	checkTraffic: function ( traffic ) { 
-		if( traffic<75 &&  myPersitencyMgr.getBool( "trafficmon.", "WarnLvl1" ) ) { //real traffic is below limit 
+	checkTraffic: function ( traffic ) {  
+		if( traffic<this.WarnLvl1-5 &&  myPersitencyMgr.getBool( "trafficmon.", "WarnLvl1" ) ) { //real traffic is below limit 
 			myPersitencyMgr.saveBool( "trafficmon.", "WarnLvl1", false ); // user should be warned for the next time he exceeds the limit
 		}
-		if( traffic>80 && !myPersitencyMgr.getBool( "trafficmon.", "WarnLvl1" ) ){ //real traffic is above limit and user has not yet been warned
-			notifier.warnLvl1("Traffic over 80%. Please be careful!");
+		if( traffic>this.WarnLvl1 && !myPersitencyMgr.getBool( "trafficmon.", "WarnLvl1" ) ){ //real traffic is above limit and user has not yet been warned
+			notifier.warnLvl1("Traffic over "+this.WarnLvl1+"%. Please be careful!");
 			myPersitencyMgr.saveBool( "trafficmon.", "WarnLvl1", true );
 		}
-		if( traffic<90 &&  myPersitencyMgr.getBool( "trafficmon.", "WarnLvl2" ) ) { //real traffic is below limit 
+		if( traffic<this.WarnLvl2 &&  myPersitencyMgr.getBool( "trafficmon.", "WarnLvl2" ) ) { //real traffic is below limit 
 			myPersitencyMgr.saveBool( "trafficmon.", "WarnLvl2", false );  // user should be warned for the next time he exceeds the limit
 		}
-		if( traffic>90 && !myPersitencyMgr.getBool( "trafficmon.", "WarnLvl2" ) ){ //real traffic is above limit and user has not yet been warned
-			notifier.warnLvl2("Traffic over 90%. Please do not exceed your traffic limit!");
+		if( traffic>this.WarnLvl2 && !myPersitencyMgr.getBool( "trafficmon.", "WarnLvl2" ) ){ //real traffic is above limit and user has not yet been warned
+			notifier.warnLvl2("Traffic over "+this.WarnLvl2+"%. Please do not exceed your traffic limit!");
 			myPersitencyMgr.saveBool( "trafficmon.", "WarnLvl2", true );
 		}
 	},   
@@ -115,7 +124,7 @@ var trafficStatus = {
 				if(traffic == -1){
 					trafficStatus.showTraffic( "Error loading page" );
 					return;
-				}
+				} 
 				trafficStatus.showTraffic( traffic );     
 			}    
 		};  
