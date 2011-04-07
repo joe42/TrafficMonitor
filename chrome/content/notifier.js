a009f36a1d246f978518547472481ca30d348b48
Johannes Müller <dna42@gmx.de>
2011-04-07 13:53:49 +0200

    fixed bug in update.rdf wich prevented updating actually committed changes that should have been in the last commit

--- chrome/content/notifier.js
+++ chrome/content/notifier.js
@@ -1,37 +1,62 @@
+/**
+ * Object to handle notifications in Firefox.
+ * 
+ * @author Johannes Müller
+ */
 var notifier = {
-	warnLvl1: function ( msg ) { 
+	/**
+	 * Gives a temporary warning with a message, like the standard "finished download"
+	 * message of firefox.
+	 * 
+	 * @author Johannes Müller 
+	 * @param msg message of the warning
+	 */
+	warnLvl1 : function(msg) {
 		//var notificationBox = gBrowser.getNotificationBox(); 
 		//notificationBox.appendNotification( msg, "warning", null, notificationBox.PRIORITY_WARNING_MEDIUM);
-		var title = "Traffic Monitor Warning";
-		try {  
-			Components.classes['@mozilla.org/alerts-service;1']  
-				  .getService(Components.interfaces.nsIAlertsService)  
-				  .showAlertNotification(null, title, msg, false, '', null)  
-		} catch(e) {  
-		    // prevents runtime error on platforms that don't implement nsIAlertsService  
-			var image = null  
-	        var win = Components.classes['@mozilla.org/embedcomp/window-watcher;1']  
-	                            .getService(Components.interfaces.nsIWindowWatcher)  
-	                            .openWindow(null, 'chrome://global/content/alerts/alert.xul',  
-	                                        '_blank', 'chrome,titlebar=no,popup=yes', null)  
-		    win.arguments = [image, title, msg, false, '']  
-		}				
-	}, 
-	warnLvl2: function ( msg ) {  
-		var nb = gBrowser.getNotificationBox() 
-		var buttons = [{
-			label: 'OK',
-			accessKey: null,    
-			callback: this.closeNotification
-		}]
+		var title = "Traffic Monitor Warning";//title of warn-message
+		try {
+			Components.classes['@mozilla.org/alerts-service;1'].getService(
+					Components.interfaces.nsIAlertsService)
+					.showAlertNotification(null, title, msg, false, '', null);
+		} catch (e) {
+			// prevents runtime error on platforms that don't implement nsIAlertsService  
+			var image = null;
+			var win = Components.classes['@mozilla.org/embedcomp/window-watcher;1']
+					.getService(Components.interfaces.nsIWindowWatcher)
+					.openWindow(null,
+							'chrome://global/content/alerts/alert.xul',
+							'_blank', 'chrome,titlebar=no,popup=yes', null);
+			win.arguments = [ image, title, msg, false, '' ];
+		}
+	},
+	/**
+	 * Gives a permanent warning with a message, like the standard popup-blocked
+	 * message of firefox. It can be removed by clicking an OK-button.
+	 * 
+	 * @author Johannes Müller
+	 * @param msg
+	 */
+	warnLvl2 : function(msg) {
+		var nb = gBrowser.getNotificationBox();
+		var buttons = [ {
+			label : 'OK',
+			accessKey : null,
+			callback : this.closeNotification
+		} ];
 
-		const priority = nb.PRIORITY_WARNING_MEDIUM
+		const
+		priority = nb.PRIORITY_WARNING_MEDIUM;
 		nb.appendNotification(msg, 'popup-blocked',
-							 'chrome://browser/skin/Info.png',
-							  priority, buttons) 
-				
-	}, 
-	closeNotification: function ( notification, buttonDesc ) {   
-		notifyBox = gBrowser.getNotificationBox().removeCurrentNotification();
+				'chrome://browser/skin/Info.png', priority, buttons);
+
+	},
+	/**
+	 * Removes a current notification.
+	 * 
+	 * @author Johannes Müller 
+	 */
+	closeNotification : function() {
+		gBrowser.getNotificationBox().removeCurrentNotification();
 	}
 }
--- chrome/content/persistencyMan.js
+++ chrome/content/persistencyMan.js
@@ -1,21 +1,109 @@
-var myPersitencyMgr = {
-	saveBool: function ( branch, name, bool ) {  
-		var prefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-		prefService.savePrefFile(null); 
-		prefService = prefService.getBranch( branch+"." );
-		prefService.QueryInterface(Components.interfaces.nsIPrefBranch2); 
-		prefService.setBoolPref( name, bool );   
-		//prefService.savePrefFile(null);
-
+/**
+ * @author Johannes Müller
+ * 
+ */
+var persistencyMgr = {
+	prefService : Components.classes["@mozilla.org/preferences-service;1"]
+			.getService(Components.interfaces.nsIPrefService),
+	observer : null,
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @param bool
+	 * @returns
+	 */
+	setBool :
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @param bool
+	 * @returns
+	 */
+	function(branch, name, bool) {
+		var branch = this.prefService.getBranch(branch + ".");
+		branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		branch.setBoolPref(name, bool);
 	},
-	getBool: function ( branch, name ) { 
-		var prefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch( branch+"." );
-		//prefService.savePrefFile(null);
- 
-		prefService.QueryInterface(Components.interfaces.nsIPrefBranch2); 
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @returns
+	 */
+	getBool : function(branch, name) {
+		var branch = this.prefService.getBranch(branch + ".");
+		branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
 		try {
-			return prefService.getBoolPref(name);
-		} catch (e) {}
+			return branch.getBoolPref(name);
+		} catch (e) {/* log error */
+		}
 		return false;
+	},
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @param str
+	 * @returns
+	 */
+	setChar : function(branch, name, str) {
+		var branch = this.prefService.getBranch(branch + ".");
+		branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		branch.setCharPref(name, str);
+	},
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @returns
+	 */
+	getChar : function(branch, name) {
+		var branch = this.prefService.getBranch(branch + ".");
+		branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		try {
+			return branch.getCharPref(name);
+		} catch (e) {/* log error */
+		}
+		return "";
+	},
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @param str
+	 * @returns
+	 */
+	setInt : function(branch, name, str) {
+		var branch = this.prefService.getBranch(branch + ".");
+		branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		branch.setIntPref(name, str);
+	},
+	/**
+	 * @author Johannes Müller
+	 * @param branch
+	 * @param name
+	 * @returns
+	 */
+	getInt : function(branch, name) {
+		var branch = this.prefService.getBranch(branch + ".");
+		branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		try {
+			return branch.getIntPref(name);
+		} catch (e) {/* log error */
+		}
+		return 0;
+	},
+	/**
+	 * @author Johannes Müller
+	 * @param aDomain
+	 * @param aObserver
+	 * @returns
+	 */
+	addObserver : function(aDomain, aObserver) {
+		observer = this.prefService.getBranch(aDomain + ".");
+		observer.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		observer.addObserver("", aObserver, false);
 	}
-}
+};
